adcdfbbf10075ef752dcc079faa15863405905c7
Jaime <jaimegonzalezfabregas@gmail.com>
2021-01-30 14:11:57 +0100

    finished

--- runtime.js
+++ runtime.js
@@ -19,7 +19,7 @@ function rand(params) {
 let playing = false;
 let waitingMode = false;
 let startOfWaiting = -1;
-let currentLevel = [];
+let currentLevel = emptyLevel();
 
 function progressBar(p) {
     //console.log(p)
@@ -35,6 +35,19 @@ function progressBar(p) {
 
     ctx.fillRect(centerX - width_ / 2, centerY - height_ / 2, width_ * p, height_);
 
+}
+
+function emptyLevel() {
+    ret = [];
+    for (let y = 0; y < 1000; y++) {
+        let row = []
+        for (let x = 0; x < 1000; x++) {
+            row.push(" ");
+        }
+        ret.push(row);
+    }
+
+    return [ret];
 }
 
 let StartLevel = -1;
@@ -57,15 +70,45 @@ var slide = false;
 var movementAditionalZ = 0;
 var falling = false;
 var acceleration = 0;
+var showingSolution = false;
+var InstructionI = 0;
+
+let levelTransitionStart = 0;
+let levelTransitionLength = 2000;
+
+let LastLevel = emptyLevel();
+
+function copy(a) {
+    if (typeof a == typeof []) {
+        let ret = [];
+        for (let i = 0; i < a.length; i++) {
+            const element = a[i];
+            ret.push(copy(element));
+        }
+        return ret;
+    } else {
+        let ret = a;
+        return ret;
+    }
+}
 
 function gameTick() {
+
+    var d = new Date();
+    let levelTransition = d.getTime() - levelTransitionStart <= levelTransitionLength;
 
     canvas.height = canvas.clientHeight;
     canvas.width = canvas.clientWidth;
     ctx.fillStyle = rgb(255, 255, 255);
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+
+
     if (!playing && LevelBackLog.length != 0) {
+        LastLevel = copy(currentLevel);
+        var d = new Date();
+        levelTransitionStart = d.getTime();
+        levelTransition = true
         currentLevel = LevelBackLog.pop();
         PlayerX = 1;
         PlayerY = 1;
@@ -97,14 +140,7 @@ function gameTick() {
 
         startOfWaiting = d.getTime();
     }
-    if (waitingMode) {
-        let totalWatingTime = NextRequestEndsAt - startOfWaiting;
-        var d = new Date();
 
-        let timeLeft = NextRequestEndsAt - d.getTime()
-        //console.log(startOfWaiting,NextRequestEndsAt,d.getTime())
-        progressBar(timeLeft / totalWatingTime);
-    }
 
     if (playing) {
         var d = new Date();
@@ -121,14 +157,25 @@ function gameTick() {
                     PlayerX = 1;
                     PlayerY = 1;
                     falling = false;
+                    OrangeFlavour = false;
                 }
             } else if (moving) {
+                if (!slide && currentLevel[0][PlayerY][PlayerX] != " ") {
+                    document.getElementById("step").play();
+                }
                 movementAditionalZ = 0;
                 slide = false;
                 moving = false;
                 //console.log(currentLevel[0][PlayerY][PlayerX]);
+
                 if (currentLevel[0][PlayerY][PlayerX] == "%") {
+                    document.getElementById("water").play();
+
                     OrangeFlavour = true;
+                }
+                if (currentLevel[0][PlayerY][PlayerX] == "~") {
+                    document.getElementById("water").play();
+
                 }
                 if (currentLevel[0][PlayerY][PlayerX] == "+") {
                     OrangeFlavour = false;
@@ -146,11 +193,11 @@ function gameTick() {
                     }
 
                 }
-                if (currentLevel[0][PlayerY][PlayerX] == "G") {
+                if (currentLevel[0][PlayerY][PlayerX] == "G" && !showingSolution) {
                     playing = false;
                 }
                 if (currentLevel[0][PlayerY][PlayerX] == "~") {
-                    if (currentLevel[0][PlayerY - 1][PlayerX] == "x" || currentLevel[0][PlayerY + 1][PlayerX] == "x" || currentLevel[0][PlayerY][PlayerX - 1] == "x" || currentLevel[0][PlayerY][PlayerX + 1] == "x") {
+                    if (OrangeFlavour || (currentLevel[0][PlayerY - 1][PlayerX] == "x" || currentLevel[0][PlayerY + 1][PlayerX] == "x" || currentLevel[0][PlayerY][PlayerX - 1] == "x" || currentLevel[0][PlayerY][PlayerX + 1] == "x")) {
                         movementStart = d.getTime();
                         PlayerXBeguin = PlayerX;
                         PlayerYBeguin = PlayerY;
@@ -166,9 +213,16 @@ function gameTick() {
                             movementDirection = "v"
 
                         }
+                        if (OrangeFlavour) {
+                            document.getElementById("pirañas").play();
+                        } else {
+                            document.getElementById("electric").play();
+
+                        }
                     }
+
                 }
-                if (currentLevel[0][PlayerY][PlayerX] == "x" || (OrangeFlavour && currentLevel[0][PlayerY][PlayerX] == "~")) {
+                if (currentLevel[0][PlayerY][PlayerX] == "x") {
                     movementStart = d.getTime();
                     PlayerXBeguin = PlayerX;
                     PlayerYBeguin = PlayerY;
@@ -184,31 +238,49 @@ function gameTick() {
                         movementDirection = "v"
 
                     }
+                    document.getElementById("electric").play();
                 }
+
+
+
             } else {
-                if (keys[38]) {
-                    movementDirection = "^"
-                    movementStart = d.getTime();
-                    PlayerXBeguin = PlayerX;
-                    PlayerYBeguin = PlayerY;
+                if (!showingSolution) {
 
-                } else if (keys[37]) {
-                    movementDirection = "<"
-                    movementStart = d.getTime();
-                    PlayerXBeguin = PlayerX;
-                    PlayerYBeguin = PlayerY;
+                    if (keys[38]) {
+                        movementDirection = "^"
+                        movementStart = d.getTime();
+                        PlayerXBeguin = PlayerX;
+                        PlayerYBeguin = PlayerY;
 
-                } else if (keys[39]) {
-                    movementDirection = ">"
-                    movementStart = d.getTime();
-                    PlayerXBeguin = PlayerX;
-                    PlayerYBeguin = PlayerY;
+                    } else if (keys[37]) {
+                        movementDirection = "<"
+                        movementStart = d.getTime();
+                        PlayerXBeguin = PlayerX;
+                        PlayerYBeguin = PlayerY;
 
-                } else if (keys[40]) {
-                    movementDirection = "v"
+                    } else if (keys[39]) {
+                        movementDirection = ">"
+                        movementStart = d.getTime();
+                        PlayerXBeguin = PlayerX;
+                        PlayerYBeguin = PlayerY;
+
+                    } else if (keys[40]) {
+                        movementDirection = "v"
+                        movementStart = d.getTime();
+                        PlayerXBeguin = PlayerX;
+                        PlayerYBeguin = PlayerY;
+                    }
+                } else {
                     movementStart = d.getTime();
                     PlayerXBeguin = PlayerX;
                     PlayerYBeguin = PlayerY;
+                    movementDirection = currentLevel[1][InstructionI];
+                    InstructionI++;
+                    if (InstructionI > currentLevel[1].length) {
+                        InstructionI = 0;
+                        PlayerX = 1;
+                        PlayerY = 1;
+                    }
                 }
             }
 
@@ -234,46 +306,119 @@ function gameTick() {
 
         for (let y = 0; y < tileZ.length; y++) {
             for (let x = 0; x < tileZ[y].length; x++) {
-                if (y == PlayerY && x == PlayerX) {
-                    var leeway = 0.05;
-                    tileZ[y][x] = tileZ[y][x] + (-tileZ[y][x] + leeway) * 0.1
+                if (!levelTransition) {
+                    if (y == PlayerY && x == PlayerX) {
+                        let leeway = 0.05;
+                        tileZ[y][x] = tileZ[y][x] + (-tileZ[y][x] + leeway) * 0.1
 
+                    } else {
+                        let goal = 0
+                        tileZ[y][x] = tileZ[y][x] + (-tileZ[y][x] + goal) * 0.1
+
+                    }
                 } else {
-                    tileZ[y][x] = tileZ[y][x] * 0.9
-
+                    let d = new Date();
+                    let levelTransitionProgress = (d.getTime() - levelTransitionStart) / levelTransitionLength;
+                    let geometricalProgress = levelTransitionProgress * Math.sqrt(Math.pow(tileZ.length / 2, 2) + Math.pow(tileZ[0].length / 2, 2));
+                    let distance = Math.sqrt(Math.pow(tileZ.length / 2 - y, 2) + Math.pow(tileZ[0].length / 2 - x, 2))
+                    let goal = 3 / (Math.pow((distance - geometricalProgress)*2, 4) + 1);
+                    tileZ[y][x] = tileZ[y][x] + (-tileZ[y][x] + goal) * 0.1
                 }
             }
+
         }
 
+        if (keys[82] && !showingSolution && !levelTransition) {
+            PlayerXBeguin = 1;
+            PlayerXBeguin = 1;
+            PlayerX = 1;
+            PlayerY = 1;
+            OrangeFlavour = false;
+            movementStart = 0;
+            slide = false;
+            moving = false;
+            movementAditionalZ = 0;
 
-        if (!justPoped && keys[83]) {
-            playing = false;
+        }
+
+        if (!justPoped && keys[83] && !levelTransition) {
+            if (showingSolution) {
+                showingSolution = false;
+                playing = false;
+                InstructionI = 0;
+                PlayerXBeguin = 1;
+                PlayerXBeguin = 1;
+                PlayerX = 1;
+                PlayerY = 1;
+                movementStart = 0;
+                slide = false;
+                moving = false;
+                movementAditionalZ = 0;
+            } else {
+                showingSolution = true;
+                OrangeFlavour = false;
+                InstructionI = 0;
+                PlayerX = 1;
+                PlayerY = 1;
+            }
+
             justPoped = true;
         }
         if (!keys[83]) {
             justPoped = false
         }
 
+
         let diagonal = Math.sqrt(Math.pow(currentLevel[0].length, 2) + Math.pow(currentLevel[0].length, 2))
 
         for (let y = 0; y < currentLevel[0].length; y++) {
             for (let x = 0; x < currentLevel[0][y].length; x++) {
 
-                ISOdraw(y, x, tileZ[y][x], currentLevel[0][y][x], diagonal);
+
+                if (levelTransition) {
+                    let d = new Date();
+                    let levelTransitionProgress = (d.getTime() - levelTransitionStart) / levelTransitionLength;
+                    let geometricalProgress = levelTransitionProgress * Math.sqrt(Math.pow(tileZ.length / 2, 2) + Math.pow(tileZ[0].length / 2, 2));
+                    let distance = Math.sqrt(Math.pow(tileZ.length / 2 - y, 2) + Math.pow(tileZ[0].length / 2 - x, 2))
+                    let drawNew = distance - geometricalProgress > 1;
+                    if (!drawNew) {
+                        ISOdraw(y, x, tileZ[y][x], currentLevel[0][y][x], diagonal);
+
+                    } else {
+                        ISOdraw(y, x, tileZ[y][x], LastLevel[0][y][x], diagonal);
+
+                    }
+                } else {
+                    ISOdraw(y, x, tileZ[y][x], currentLevel[0][y][x], diagonal);
+
+                }
+
 
                 if (Math.ceil(PlayerY) == y && x == Math.ceil(PlayerX)) {
                     playerZ = -0.32 + tileZ[Math.round(PlayerY)][Math.round(PlayerX)] - movementAditionalZ
-
-                    if (!OrangeFlavour) {
-                        ISOdraw(PlayerY, PlayerX, playerZ, "Player", diagonal);
-                    } else {
-                        ISOdraw(PlayerY, PlayerX, playerZ, "PlayerO", diagonal);
+                    if (!levelTransition) {
+                        if (!OrangeFlavour) {
+                            ISOdraw(PlayerY, PlayerX, playerZ, "Player", diagonal);
+                        } else {
+                            ISOdraw(PlayerY, PlayerX, playerZ, "PlayerO", diagonal);
+                        }
                     }
+
                 }
             }
         }
 
 
+    }
+
+
+    if (waitingMode) {
+        let totalWatingTime = NextRequestEndsAt - startOfWaiting;
+        var d = new Date();
+
+        let timeLeft = NextRequestEndsAt - d.getTime()
+        //console.log(startOfWaiting,NextRequestEndsAt,d.getTime())
+        progressBar(timeLeft / totalWatingTime);
     }
 
 
